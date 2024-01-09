286250fa627d5e739ab502baf276ada295019110
46707 <douglas_sebom@msn.com>
2024-01-10 00:21:04 +0100

    top banner added

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,34 +1,84 @@
 //Get hard coded Stockholm to start with
+//Conditional rendering is needed to be learnt
+//Make an object to put weather state in instead of arrays
 
-const TopBar = () => {
+import { useRef, useState, useEffect } from "react";
+import constants from "../constants/constants";
+import WeatherData from "../classes/WeatherData";
+import '../styles/topbar.css';
+import nightImg from "../assets/nighttime.png";
+import dayImg from "../assets/daytime.png";
+
+const TopBar = (props) => {
 
     //style
 
     //state & refs
     const cityInput = useRef();
+    //<input ref={cityInput}></input>
+    
+    const [city, setCity] = useState(props.city);
+    const [weatherData, setWeather] = useState({Time: "", Img: "", Temp: ""});
+    const [nightTime, setDayTime] = useState(false);
+    const [todBanner, setBanner] = useState(dayImg);
 
     //functions
-    const getCountryData = () => {
-        const geoLocation = "";
-        const url = `https://api.weatherstack.com/current?access_key=${YOUR_ACCESS_KEY}&query=${city}`;
-        return(
-            <div>
-                this is some weather data
-            </div>
-        )
     
+    useEffect(() => {
+        (async function getCountryData () {
+        const geoLocation = "";
+                const url = `http://api.weatherapi.com/v1/current.json?key=${constants}&q=${city}`;
+                return await fetch(url, {referrerPolicy: "unsafe-url"}).then(res => res.json())
+                .then(res => {
+                    if(res != undefined){
+                        const weather = new WeatherData(res.current.last_updated, res.current.temp_c, res.current.condition.icon)
+                        setWeather(weather);
+                        //changeBanner();
+                    }
+                });
+        })();
+            
+    }, []);
+
+    useEffect(() => {
+        changeBanner(); // Access the updated value here
+    }, [weatherData]);
+    
+        //getCountryData();
+    const changeBanner = () => {
+        //it should check for it its night time
+        weatherData.Img.includes("night") ? setBanner(nightImg) : setBanner(dayImg);
+        //this one sets the css!
+        //console.log("Is it night?" + nightTime);
+        console.log(todBanner);
     }
+    
+
+    //wash data
 
     //mapping
-
+    
 
     //main return
 
     return(
         <>
-            <input ref={cityInput}></input>
-            <button onClick={getCountryData}></button>
-            {getCountryData()}
+            
+            <div className="maindiv" style={{backgroundImage: `url(${todBanner})`}}>
+                <div className="subdiv">
+                    <p>
+                        This is some weather data for {city}
+                    </p>
+                    <p>
+                        Temperature: {weatherData.Temp} C, Time: {weatherData.Time}
+                    </p>
+                    <p>
+                        <img src={weatherData.Img}></img>
+                    </p>
+                    <button>See details</button>
+
+                </div>
+            </div>
         </>
     )
 
