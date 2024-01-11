f2b36c12b430c414c5f35aa51aabc749d76657b1
46707 <douglas_sebom@msn.com>
2024-01-11 07:57:42 +0100

    tracked constants

---
+++ src/constants/constants.js
@@ -0,0 +1,34 @@
+//const apiKey = "297e791e3cfaf477a0e9486e9511e796";
+const apiKey = "13486d447f0c4713ac5201112240901";
+
+const startDetailValues = [{
+    Date: "",
+    Max: "",
+    Min: "",
+    Img: ""
+},{
+    Date: "",
+    Max: "",
+    Min: "",
+    Img: ""
+},{
+    Date: "",
+    Max: "",
+    Min: "",
+    Img: ""
+},{
+    Date: "",
+    Max: "",
+    Min: "",
+    Img: ""
+},{
+    Date: "",
+    Max: "",
+    Min: "",
+    Img: ""
+}]
+
+export {
+    apiKey,
+    startDetailValues
+}
